9714e0646bdd7e41c368ab449700201cb1d5ec3d
bbworld1 <vwangsf@gmail.com>
2023-10-07 17:05:01 -0400

    Basic trajectory reading/writing to/from PSRAM

--- include/Trajectory.hpp
+++ include/Trajectory.hpp
@@ -1,22 +1,50 @@
 #ifndef TRAJECTORY_H
 #define TRAJECTORY_H
+#include <cstdint>
 
-class TrajectoryPoint {
-public:
-    float K[24][4]; // Gains matrix for trajectory error
+// 4MB trajectory memory buffer
+extern uint8_t TRAJECTORY_MEM[];
+
+// Size: 1024B max;
+typedef struct {
+    int p; // # Trajectory segments = # Gain Matrices
+    int kDim1; // First dimension of gain matrix
+    int kDim2; // Second dimension of gain matrix
+    int k; // # Trajectory points total
+} TrajectoryHeader;
+
+typedef struct {
+    float *K; // K gains matrix (variable dims)
+} Gains;
+
+// Size: 96B;
+typedef struct {
     float x[12]; // State trajectory vector
-    float u_ref[4]; // "Reference" inputs (what is nominally needed to get here from previous state)
+    float uRef[4]; // "Reference" inputs (what is nominally needed to get here from previous state)
+    float time;
+} TrajectoryPoint;
+
+enum TrajectoryStatus {
+    TRAJECTORY_OK = 0,
+    TRAJECTORY_NOT_ENOUGH_SPACE = -1,
+    TRAJECTORY_NOT_ENOUGH_GAINS_SPACE = -2,
 };
 
 class Trajectory {
 public:
-    TrajectoryPoint getTrajectoryPoint(int k); // Fetches trajectory point k
-    void storeTrajectoryPoint(); // Store a trajectory point at end of trajectory block
-    void storeTrajectoryPoint(int k); // Store a trajectory point at position k if it exists (overwrites existing)
-    void deleteTrajectoryData(); // Delete all trajectory data
-    void deleteTrajectoryPoint(int k); // Delete trajectory point k
-private:
-    TrajectoryPoint trajectoryBuffer[8]; // Hold next few trajectory points
+    Trajectory();
+    void wipe(); // Wipes any existing trajectory
+    int init(int p, int kDim1, int kDim2, int k); // Initializes trajectory header block with data
+    int readFromSD(); // Read trajectory data from SD card. Requires SD initialized beforehand.
+    TrajectoryPoint* getTrajectoryPoint(int k); // Fetches trajectory point k
+    void appendGainsMatrix(float *K); // Store a gains matrix
+    void storeGainsMatrix(float *K, int p); // Store a gains matrix
+    void appendTrajectoryPoint(TrajectoryPoint *point); // Store a trajectory point at end of trajectory block
+    void storeTrajectoryPoint(TrajectoryPoint *point, int k); // Store a trajectory point at position k if it exists
+    int kDim1;
+    int kDim2;
+    int gainsCount;
+    int pointsCount;
 };
 
 #endif // TRAJECTORY_H
--- src/Trajectory.cpp
+++ src/Trajectory.cpp
@@ -1,1 +1,67 @@
-#include "Trajectory.h"
+#include <Arduino.h>
+#include "Trajectory.hpp"
+#include <cstring>
+
+#define TRAJECTORY_MEM_SIZE_KB 256
+
+EXTMEM uint8_t TRAJECTORY_MEM[TRAJECTORY_MEM_SIZE_KB * 1024];
+
+Trajectory::Trajectory() {
+    this->gainsCount = 0;
+    this->pointsCount = 0;
+}
+
+void Trajectory::wipe() {
+    for (int i = 0; i < TRAJECTORY_MEM_SIZE_KB * 1024; i++)
+    TRAJECTORY_MEM[i] = 0;
+}
+
+int Trajectory::init(int p, int kDim1, int kDim2, int k) {
+    TrajectoryHeader *header = (TrajectoryHeader*) TRAJECTORY_MEM;
+
+    // Check bounds
+    if (p * 384 > 3840) return TRAJECTORY_NOT_ENOUGH_GAINS_SPACE;
+    if (k * 72 > (TRAJECTORY_MEM_SIZE_KB * 1024 - 4864))
+        return TRAJECTORY_NOT_ENOUGH_SPACE;
+    header->p = p;
+    header->k = k;
+    header->kDim1 = kDim1;
+    header->kDim2 = kDim2;
+
+    return TRAJECTORY_OK;
+}
+
+TrajectoryPoint* Trajectory::getTrajectoryPoint(int k) {
+    TrajectoryPoint* ptr = (TrajectoryPoint*) (TRAJECTORY_MEM + 4864);
+    ptr += k;
+    return ptr;
+}
+
+void Trajectory::appendGainsMatrix(float *K) {
+    this->storeGainsMatrix(K, this->gainsCount);
+    this->gainsCount += 1;
+}
+
+void Trajectory::storeGainsMatrix(float *K, int p) {
+    float* ptr = (float*) (TRAJECTORY_MEM + 1024);
+    ptr += this->kDim1 * this->kDim2 * p;
+    memcpy(ptr, K, sizeof(float) * this->kDim1 * this->kDim2);
+}
+
+void Trajectory::appendTrajectoryPoint(TrajectoryPoint *point) {
+    this->storeTrajectoryPoint(point, this->pointsCount);
+    this->pointsCount += 1;
+}
+
+void Trajectory::storeTrajectoryPoint(TrajectoryPoint *point, int k) {
+    TrajectoryPoint *ptr = this->getTrajectoryPoint(k);
+    // Serial.printf("Write to %p\n", ptr);
+    memcpy(ptr->x, point->x, 12 * sizeof(float));
+    memcpy(ptr->uRef, point->uRef, 4 * sizeof(float));
+    ptr->time = point->time;
+    // Serial.printf("Store point %d: x{", k);
+    // for (int j = 0; j < 12; j++) Serial.printf("%d,", point->x[j]);
+    // Serial.printf("}, uRef{");
+    // for (int j = 0; j < 4; j++) Serial.printf("%d,", point->uRef[j]);
+    // Serial.printf("}, time: %d\n", point->time);
+}
