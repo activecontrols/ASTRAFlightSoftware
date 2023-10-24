099e85d092ba3cfb3ba7de728dd7cdcbc4bd5599
Vincent Palmerio <vpalmerio@protonmail.com>
2023-10-24 18:43:47 -0400

    Bug fix on integration and derivative testing and matrix multiplication is 24x24

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,40 @@
 #include "../lib/math/Derivative.h"
 //#include <ArduinoEigenDense.h>
 
-Eigen::MatrixXd m(3, 3);
-Eigen::VectorXd v(3);
+Eigen::MatrixXd m(24, 24);
+Eigen::VectorXd v(24);
 
 Eigen::VectorXd integrateAndDerive(3);
 
 elapsedMillis ledTime;
 
+elapsedMillis timeToEnd;
+
 bool ledOn = false;
+
+int controllerErrorCode = -20;
+int estimatorErrorCode = -20;
+int integratorErrorCode = -20;
+int derivativeErrorCode = -20;
 
 void setup() {
   Serial.print("Controller error code:");
-  Serial.print(controllerSetup());
+  controllerErrorCode = controllerSetup();
+  Serial.println(controllerErrorCode);
 
   Serial.print("Estimator error code:");
-  Serial.print(estimatorSetup());
+  estimatorErrorCode = estimatorSetup();
+  Serial.println(estimatorErrorCode);
 
   integrateAndDerive << 1, 2, 3;
 
   Serial.print("Integrator error code:");
-  Serial.print(integratorSetup(&integrateAndDerive));
+  integratorErrorCode = integratorSetup(&integrateAndDerive);
+  Serial.println(integratorErrorCode);
 
   Serial.print("Derivative error code:");
-  Serial.print(derivativeSetup(&integrateAndDerive));
+  derivativeErrorCode = derivativeSetup(&integrateAndDerive, integrateAndDerive);
+  Serial.println(derivativeErrorCode);
   
   //Sets up led
   pinMode(LED_BUILTIN, OUTPUT);
@@ -44,38 +55,31 @@ void setup() {
   float theta = PI/2;
 
 
-  
-  m(0, 0) = 1;
-  m(1, 0) = 0;
-  m(2, 0) = 0;
-  m(0, 1) = 0;
-  m(0, 2) = 0;
-  m(1, 1) = cos(theta);
-  m(1, 2) = -sin(theta);
-  m(2, 1) = sin(theta);
-  m(2, 2) = cos(theta);
+  //ALWAYS INITIALIZE EACH VALUE IN VECTORS AND MATRICES
+  for (int i = 0; i < 24; i++) {
+    for (int j = 0; j < 24; j++) {
+      m(i, j) = 2;
+    }
+  }
+
+  for (int i = 0; i < 24; i++) {
+    v(i) = 2;
+  }
 
   //Undefined Vector Test
-  Serial.println("Vector w/o initialized values: ");
-  Serial.print(v(0));
-  Serial.println();
-  Serial.print(v(1));
-  Serial.println();
-  Serial.print(v(2));
-  Serial.println();
-
-  v(0) = 0;
-  v(1) = 1;
-  v(2) = 0;
-   
-  
-
+  // Serial.println("Vector w/o initialized values: ");
+  // Serial.print(v(0));
+  // Serial.println();
+  // Serial.print(v(1));
+  // Serial.println();
+  // Serial.print(v(2));
+  // Serial.println();
 }
 
 //turns the LED on and off every 3 seconds 
 void led() {
   
-  if (ledTime == 3000) {
+  if (ledTime >= 3000) {
 
     //(HIGH and LOW are the voltage levels)
     if (ledOn == true) {
@@ -86,44 +90,78 @@ void led() {
       ledOn = true;
     }
     
+    // Serial.println("LED Time");
+    // Serial.println(ledTime);
 
-    ledTime -= 3000;
+    ledTime = 0;
   }
 }
 
 void loop() {
   
-  
+  //Matrix Multiplication test
   Eigen::VectorXd vo = m * v;
   v = vo;
 
-  integrateAndDerive *= 2;
 
+  // Serial.print("Controller error code:");
+  // Serial.println(controllerErrorCode);
+
+  // Serial.print("Estimator error code:");
+  // Serial.println(estimatorErrorCode);
+
+  // Serial.print("Integrator error code:");
+  // Serial.println(integratorErrorCode);
+
+  // Serial.print("Derivative error code:");
+  // Serial.println(derivativeErrorCode);
+
+  //integrateAndDerive += 2;
+  //2, 4, 6
+
+  
+  integrateAndDerive(0) = integrateAndDerive(0) + 2;
+  integrateAndDerive(1) = integrateAndDerive(1) + 2;
+  integrateAndDerive(2) = integrateAndDerive(2) + 2;
+
+  Serial.println("Main.cpp Vector");
+  for (int i = 0; i < integrateAndDerive.size(); i++) {
+    Serial.println(integrateAndDerive(i));
+  }
+
+  Serial.println("Integration data");
   integratorUpdate();
   Serial.println("Integration");
   for (int i = 0; i < integratedData.size(); i++) {
-    Serial.println(integratedData(i));
+    Serial.println(integratedData(i), 60);
   }
 
+  Serial.println("Derivative data");
   derivativeUpdate();
   Serial.println("Derivative");
   for (int i = 0; i < derivative.size(); i++) {
-    Serial.println(derivative(i));
+    Serial.println(derivative(i), 60);
   }
 
-  Serial.println("New Vector: ");
-  Serial.print(v(0));
-  Serial.println();
-  Serial.print(v(1));
-  Serial.println();
-  Serial.print(v(2));
-  Serial.println();
+  // Serial.println("New Vector: ");
+  // Serial.print(v(0));
+  // Serial.println();
+  // Serial.print(v(1));
+  // Serial.println();
+  // Serial.print(v(2));
+  // Serial.println();
 
   //turns led on and off
   led();
   
 
   // wait for a second
-  delay(1000);
+  //delay(4000);
+
+  //end program after 100 millis
+  if (timeToEnd >= 100) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(20000000);
+  }
 }
 
