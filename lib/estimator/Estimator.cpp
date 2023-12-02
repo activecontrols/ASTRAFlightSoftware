7c9435f0c261b0da36d167a3a8c899cf02115cfd
vpalmerio <vpalmerio@protonmail.com>
2023-12-02 15:02:16 -0500

    Calculate estimatedStateX with updateEstimator()

--- lib/estimator/Estimator.cpp
+++ lib/estimator/Estimator.cpp
@@ -1,20 +1,37 @@
 #include "Estimator.h"
-#include <stdlib.h>
+#include "../math/Integrator.h"
 #include "../error/Error.h"
+#include "../drivers/ASTRA/IMU/src/IMU.h"
+#include <ArduinoEigenDense.h>
 
-//variables may need to be included outside the function in order to be defined globally instead of locally
-//research using extern
-float* estimatedStateX = (float *)malloc(STATE_DIMENSION * sizeof(float));
+Eigen::VectorXd estimatedStateX(STATE_DIMENSION);
+Integrator linearAccelIntegrator;
 
 int initializeEstimator() {
 
-    if (estimatedStateX == NULL) {
-        return MEMORY_ALLOCATION_ERROR_CODE;
+    for (unsigned int i = 0; i < STATE_DIMENSION; i++) {
+        estimatedStateX[i] = 0;
     }
 
-    for (unsigned int i = 0; i < STATE_DIMENSION; i++) {
-        estimatedStateX[i] = 1;
-    }
+    linearAccelIntegrator.integratorSetup(&linearAccelVector);
     
+    return NO_ERROR_CODE;
+}
+
+int updateEstimator() {
+    updateIMU();
+    linearAccelIntegrator.integratorUpdate();
+
+    estimatedStateX(0) = qx;
+    estimatedStateX(1) = qy;
+    estimatedStateX(2) = qz;
+
+    estimatedStateX(3) = gx;
+    estimatedStateX(4) = gy;
+    estimatedStateX(5) = gz;
+
+    estimatedStateX(6) = linearAccelIntegrator.integratedData(0);
+
+
     return NO_ERROR_CODE;
 }
