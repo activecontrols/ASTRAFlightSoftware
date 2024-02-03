941fde502f264e74ce0fa130df49f0cf136bf653
vpalmerio <vpalmerio@protonmail.com>
2024-02-03 02:41:08 -0500

    Add mission timer library

---
+++ lib/timer/timer.h
@@ -0,0 +1,22 @@
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <elapsedMillis.h>
+#include "Error.h"
+
+#define MICROSECONDS_IN_A_SECOND (1000000)
+
+
+/*
+ * Starts mission timer. Timer is in microseconds
+ */
+extern int startMissionTimer();
+
+/*
+ * Returns mission time in seconds, CANNOT_REQUEST_MISSION_TIME_IF_MISSION_NOT_STARTED if
+ * function is called before mission is started.
+ */
+extern float getMissionTimeSeconds();
+
+#endif
