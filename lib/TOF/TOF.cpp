23f0ad85dd7d7c3bd35f98acc9ae7cc8d1ca0623
vpalmerio <vpalmerio@protonmail.com>
2024-03-15 10:16:09 -0500

    TOF now works correctly with buffer, more modifications needed

--- lib/TOF/TOF.cpp
+++ lib/TOF/TOF.cpp
@@ -1,4 +1,4 @@
-#include <TOF.h>
+#include "TOF.h"
 
 /*
 TOF.cpp 
@@ -12,12 +12,12 @@ Last updated: 2/21/2024
 Buffer TOF_buffer(1, NUM_TOF_MEASUREMENTS, updateTOFDistance);
 volatile int interruptCount;
 VL53L4CX sensor_vl53l4cx_sat;
-float* latestData;
+float lastData;
 
 int setupTOFSensor(unsigned int interruptPin, unsigned int XSHUTPin) {
     interruptCount = 0;
-    float initialData = 0;
-    latestData = &initialData;
+    //float initialData = 0;
+    //latestData = &initialData;
 
     sensor_vl53l4cx_sat = VL53L4CX(&DEV_I2C, XSHUTPin);
 
@@ -33,9 +33,14 @@ int setupTOFSensor(unsigned int interruptPin, unsigned int XSHUTPin) {
 
     // Switch off VL53L4CX satellite component.
     sensor_vl53l4cx_sat.VL53L4CX_Off();
+    
 
     // Initialize VL53L4CX satellite component.
     status = sensor_vl53l4cx_sat.InitSensor(0x29);
+    //sensor_vl53l4cx_sat.VL53L4CX_On();
+    // sensor_vl53l4cx_sat.VL53L4CX_WaitDeviceBooted();
+    // status = sensor_vl53l4cx_sat.VL53L4CX_DataInit();
+    //status = 0;
 
     sensor_vl53l4cx_sat.VL53L4CX_StartMeasurement();
 
@@ -43,31 +48,39 @@ int setupTOFSensor(unsigned int interruptPin, unsigned int XSHUTPin) {
 }
 
 float* updateTOFDistance() {
-    if (interruptCount == 1) {
+    float* latestData = (float*)malloc(1 * sizeof(float));
+    latestData[0] = lastData;
+    Serial.println("test");
+    //if (interruptCount == 1) {
         interruptCount = 0;
-        float* temp = (float*)malloc(1 * sizeof(float));
+        
         VL53L4CX_MultiRangingData_t MultiRangingData;
         VL53L4CX_MultiRangingData_t *pMultiRangingData = &MultiRangingData;
-        uint8_t NewDataReady = 0;
-        int no_of_object_found = 0, j;
-
+        uint8_t NewDataReady = 1;
+        int no_of_object_found = 0;
+        Serial.println("test2");
         int status = sensor_vl53l4cx_sat.VL53L4CX_GetMeasurementDataReady(&NewDataReady);
-
-        if (!status) {
+        Serial.println("test3");
+        if (status) {
             status = sensor_vl53l4cx_sat.VL53L4CX_GetMultiRangingData(pMultiRangingData);
 
-            temp[0] = pMultiRangingData->RangeData[j].RangeMilliMeter / 1000.0;
-            Serial.println(pMultiRangingData->RangeData[j].RangeMilliMeter / 1000.0);
+            lastData = pMultiRangingData->RangeData[0].RangeMilliMeter / 1000.0;
+            latestData[0] = lastData;
+            Serial.print("TOF: ");
+            Serial.println(pMultiRangingData->RangeData[0].RangeMilliMeter / 1000.0);
 
             if (status == 0) {
                 status = sensor_vl53l4cx_sat.VL53L4CX_ClearInterruptAndStartMeasurement();
             }
         } else {
+            Serial.println("here4");
             return latestData;
         }
-    } else {
-        return latestData;
-    }
+    //} else {
+    //    Serial.println("here3");
+    //    return latestData;
+    //}
+    Serial.println("here2");
     return latestData;
 }
 
