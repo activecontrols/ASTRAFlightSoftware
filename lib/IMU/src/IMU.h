3670d17de2196ea4159c84bddd2d2688f6d5422d
vpalmerio <vpalmerio@protonmail.com>
2024-04-20 23:28:41 -0400

    Increase filter update rate to fix control input issue

--- lib/IMU/src/IMU.h
+++ lib/IMU/src/IMU.h
@@ -41,7 +41,7 @@ Last updated: 11/4/2023
   Adafruit_Sensor_Calibration_SDFat cal;
 #endif
 
-#define FILTER_UPDATE_RATE_HZ (100)
+#define FILTER_UPDATE_RATE_HZ (10000)
 #define PRINT_EVERY_N_UPDATES (10)
 #define IMU_WIRE Wire
 //#define AHRS_DEBUG_OUTPUT
