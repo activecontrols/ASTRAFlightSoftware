17b09e9e3ffaca61b0d8c2fbfdfe2b407e391b70
Vincent Wang <vwangsf@gmail.com>
2024-10-15 11:33:32 -0400

    Use vector instead

--- lib/flightmodule/Scheduler.cpp
+++ lib/flightmodule/Scheduler.cpp
@@ -4,16 +4,15 @@
  * Construct a scheduler with a list of modules.
  * Must pass correct size.
  */
-Scheduler::Scheduler(FlightModule schedule[], int size) {
+Scheduler::Scheduler(std::vector<FlightModule> schedule) {
     this->schedule = schedule;
-    this->size = size;
 }
 
 /**
  * Call inits for each module.
  */
 Scheduler::init() {
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < this->schedule.size(); ++i) {
         this->schedule[i].init();
     }
 }
@@ -22,7 +21,7 @@ Scheduler::init() {
  * Call updates for each module.
  */
 Scheduler::update(unsigned long time) {
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < this->schedule.size(); ++i) {
         this->schedule[i].update();
     }
 }
--- lib/flightmodule/Scheduler.h
+++ lib/flightmodule/Scheduler.h
@@ -1,12 +1,12 @@
 /**
  * The Scheduler is responsible for running a schedule of FlightModules.
  */
+#include <vector>
 #include <flightmodule/FlightModule.h>
 
 class Scheduler : public FlightModule {
 public:
-    Scheduler(FlightModule *schedule, int size);
+    Scheduler(std::vector<FlightModule> schedule);
 private:
-    FlightModule *schedule;
-    int size = 0;
+    std::vector<FlightModule> schedule;
 };
