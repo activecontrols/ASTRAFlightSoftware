376029987586d13e1bbf9172152e0de1234cae9f
vpalmerio <vpalmerio@protonmail.com>
2024-11-16 04:42:37 -0500

    Created ControllerModule class

---
+++ lib/controller/ControllerModule.cpp
@@ -0,0 +1,97 @@
+#include "ControllerModule.h"
+
+int Controller::init() {
+
+    modes[STABILIZE_MODE] = (ControlMode*) &stabilize;
+
+#if IS_ARDUINO
+  #if ENABLE_MOTOR_CONTROL
+    innerGimbal.attach(INNER_GIMBAL_PIN);
+    outerGimbal.attach(OUTER_GIMBAL_PIN);
+    torqueVaneLeft.attach(LEFT_TORQUE_VANE_PIN);
+    torqueVaneRight.attach(RIGHT_TORQUE_VANE_PIN);
+
+    if (!innerGimbal.attached()) {
+      return INNER_GIMBAL_NOT_ATTACHED;
+    }
+
+    if (!outerGimbal.attached()) {
+      return OUTER_GIMBAL_NOT_ATTACHED;
+    }
+
+    if (!torqueVaneLeft.attached()) {
+      return LEFT_TORQUE_VANE_NOT_ATTACHED;
+    }
+
+    if (!torqueVaneRight.attached()) {
+      return RIGHT_TORQUE_VANE_NOT_ATTACHED;
+    }
+
+    innerGimbal.write(INNER_GIMBAL_INITIAL_SETTING);
+    outerGimbal.write(OUTER_GIMBAL_INITIAL_SETTING);
+    torqueVaneLeft.write(LEFT_TORQUE_VANE_INITIAL_SETTING);
+    torqueVaneRight.write(RIGHT_TORQUE_VANE_INITIAL_SETTING);
+  #endif
+
+  #if USE_EDF
+    if (!edfFan.checkAttached()) {
+      return ESC_NOT_ATTACHED;
+    }
+  #endif
+#endif
+
+  return 0;
+}
+
+void Controller::update() {
+    //call current control law based on value in flight data
+    modes[flightData::currentMode]->update();
+
+    dampenControls();
+    saturate();
+    controlServos();
+}
+
+void Controller::dampenControls() {
+#if IS_ARDUINO
+  #if USE_ENCODER
+    float beta_dot = encoder::magEncoder1.getAngularSpeed();
+    float gamma_dot = encoder::magEncoder2.getAngularSpeed();
+
+    //TODO: servo dampening needs to happen for the vanes
+
+    controllerInputU(1) = controllerInputU(1) + BETA_DAMPENING_CONSTANT*beta_dot;
+    controllerInputU(0) = controllerInputU(0) + GAMMA_DAMPENING_CONSTANT*gamma_dot;
+  #endif
+#endif
+}
+
+void Controller::saturate() {
+  //beta
+  flightData::controllerInputU(0) = constrain(flightData::controllerInputU(0), BETA_MIN, BETA_MAX);
+
+  //gamma
+  flightData::controllerInputU(1) = constrain(flightData::controllerInputU(1), GAMMA_MIN, GAMMA_MAX);
+
+  //throttle
+  flightData::controllerInputU(2) = constrain(flightData::controllerInputU(2), THROTTLE_MIN, THROTTLE_MAX);
+
+  //alpha
+  flightData::controllerInputU(3) = constrain(flightData::controllerInputU(3), ALPHA_MIN, ALPHA_MAX);
+}
+
+void Controller::controlServos() {
+#if IS_ARDUINO
+  #if ENABLE_MOTOR_CONTROL
+    innerGimbal.write(controllerInputU(0) + INNER_GIMBAL_INITIAL_SETTING); //write gamma to inner gimbal
+    outerGimbal.write(controllerInputU(1) + OUTER_GIMBAL_INITIAL_SETTING); //write beta to outer gimbal
+    
+    torqueVaneLeft.write(controllerInputU(3) + LEFT_TORQUE_VANE_INITIAL_SETTING); //write alpha to left torque vane
+    torqueVaneRight.write(-controllerInputU(3) + RIGHT_TORQUE_VANE_INITIAL_SETTING); //write -alpha to right torque vane
+  #endif
+
+  #if USE_EDF
+    int errorCode = edfFan.setThrottle(controllerInputU(2)); //write throttle to ESC (power), which then talks to EDF (fan))
+  #endif
+#endif
+}
---
+++ lib/controller/ControllerModule.h
@@ -0,0 +1,74 @@
+#ifndef CONTROLLER_MODULE_H
+#define CONTROLLER_MODULE_H
+
+#include "FlightModule.h"
+#include "ControlMode.h"
+#include "StabilizeController.h"
+#include "Error.h"
+
+#if IS_ARDUINO
+    #include <Servo.h>
+    #include "ESC.h"
+#endif
+
+/** Constants to constrain control values */
+#define BETA_MAX (8) /* outer gimbal max*/
+#define BETA_MIN (-BETA_MAX) /* outer gimbal min */
+#define GAMMA_MAX (8) /* inner gimbal max */
+#define GAMMA_MIN (-GAMMA_MAX) /* inner gimbal min */
+#define THROTTLE_MIN (0) /* Must change in ESC.cpp (cannot include controller.h because circular dependencies)*/
+#define THROTTLE_MAX (1) /* Must change in ESC.cpp (cannot include controller.h because circular dependencies)*/
+#define ALPHA_MAX (8) /* left and right torque vane max */
+#define ALPHA_MIN (-ALPHA_MAX) /* left and right torque vane min */
+
+/** Pins for servos and ESC */
+#define INNER_GIMBAL_PIN (29)
+#define OUTER_GIMBAL_PIN (39)
+#define LEFT_TORQUE_VANE_PIN (33)
+#define RIGHT_TORQUE_VANE_PIN (36)
+#define ESC_PIN (9)
+
+/** Initial settings for servos */
+#define INNER_GIMBAL_INITIAL_SETTING (82)
+#define OUTER_GIMBAL_INITIAL_SETTING (110)
+#define LEFT_TORQUE_VANE_INITIAL_SETTING (140)
+#define RIGHT_TORQUE_VANE_INITIAL_SETTING (140)
+
+namespace flightData {
+    extern Eigen::VectorXd controllerInputU;
+    extern CONTROL_MODE currentMode;
+}
+
+class Controller : FlightModule {
+/**
+ * Module to handle switching between control modes and updating the current control mode's respective control loop
+ * @author Vincent Palmerio <vpalmeri@purdue.edu>
+ */
+
+  Servo innerGimbal;
+  Servo outerGimbal;
+  Servo torqueVaneLeft;
+  Servo torqueVaneRight;
+
+  ESC edfFan = ESC(ESC_PIN);
+  Stabilize stabilize = Stabilize(CONTROL_MODE::STABILIZE_MODE);
+
+public:
+
+  int init(); /** initialize */
+  void update(); /** check switch to different control mode is needed, else update control loop of current control mode */
+  bool often = true;
+  int every_us = 200;
+  int timeout_ms = 20;
+  
+private:
+
+  void saturate();
+  void controlServos();
+  void dampenControls();
+
+  ControlMode* modes[4];
+
+};
+
+#endif // CONTROLLER_MODULE_H
