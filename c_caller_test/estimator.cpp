c90821e29228480af1d2ff547a3c022c5af6d8e8
Jeffrey Wu <jeffthereff2017@gmail.com>
2025-02-01 16:24:05 -0500

    slightly working code without estimator

--- c_caller_test/estimator.cpp
+++ c_caller_test/estimator.cpp
@@ -3,13 +3,13 @@
 #include "MEKF.h"
 #include "MEKFEstimatorModule.h"
 
-void run(double* input, double* time, double* output) {
-    // Eigen::VectorXd measurementVectorY = toEigen(input, 6);
-    // Eigen::VectorXd estimatedStateX;
-    // update((long) time * 1e9);
-    // memcpy(output, toArray(estimatedStateX), sizeof(double) * 6);
-    memcpy(output, input, sizeof(double) * 6);
-}
+namespace flightData {
+    Eigen::VectorXd measurementVectorY = Eigen::VectorXd(6);
+    Eigen::VectorXd estimatedStateX = Eigen::VectorXd(6);
+};
+
+Eigen::VectorXd toEigen(double*);
+double* toArray(Eigen::VectorXd);
 
 Eigen::VectorXd toEigen(double* arr, int size) {
     return Eigen::VectorXd::Map(arr, size);
@@ -17,4 +17,19 @@ Eigen::VectorXd toEigen(double* arr, int size) {
 
 double* toArray(Eigen::VectorXd eigen) {
     return eigen.data();
+}
+
+long t = 1e9;
+// auto estimator = MEKFEstimatorModule::MEKFEstimatorModule();
+
+void init() {
+    // estimator.init();
+}
+
+void run(double* input, double* output) {
+    flightData::measurementVectorY = toEigen(input, 6);
+    // estimator.update(t);
+    // t += 1e9;
+    memcpy(output, toArray(flightData::measurementVectorY), sizeof(double) * 6);
+    // memcpy(output, input, sizeof(double) * 6);
 }
--- c_caller_test/estimator.h
+++ c_caller_test/estimator.h
@@ -1,6 +1,5 @@
 #ifndef ESTIMATOR_H_INCLUDED
 #define ESTIMATOR_H_INCLUDED
-void run(double* input, double time, double *output);
-Eigen::VectorXd toEigen(double* arr);
-double* toArray(Eigen::VectorXd eigen, int size);
+void init();
+void run(double*, double*);
 #endif // ESTIMATOR_H
